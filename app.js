69601d4af3c9f614f39411585eed5f8d07e090d9
risq <valentin.ledrapier@gmail.com>
2015-09-29 14:33:49 +0200

    Working socket io connection to retrieve orientation

--- app.js
+++ app.js
@@ -1,57 +1,23 @@
-var five = require("johnny-five");
+var five = require('johnny-five');
+var io = require('socket.io-client');
+
+var plane = require('./plane.js');
+
 var board = new five.Board();
 var servo, slider;
 
+var socket = io('http://192.168.31.92:5000');
+
+socket.on('connected', function() {
+  console.log('connected');
+  socket.emit('arduino');
+});
+
+socket.on('orientation', function(data) {
+  console.log('orientation', data);
+  plane.applyRotation(data.gamma, data.beta);
+});
+
 board.on("ready", function() {
-
-  var scalingRange = [0, 170]
-
-  servo = {
-    nw: new five.Servo({
-      pin: 10,
-      range: scalingRange
-    }),
-    ne: new five.Servo({
-      pin: 11,
-      range: scalingRange
-    }),
-    sw: new five.Servo({
-      pin: 12,
-      range: scalingRange
-    }),
-    se: new five.Servo({
-      pin: 13,
-      range: scalingRange
-    }),
-  }
-
-  slider = new five.Sensor({
-    pin: "A0",
-    freq: 50
-  });
-
-  // slider.scale(scalingRange).on("slide", function(err, value) {
-  //   console.log(value);
-  //   servo.nw.to(Math.floor(this.value));
-  //   servo.ne.to(Math.floor(this.value));
-  //   servo.sw.to(Math.floor(this.value));
-  //   servo.se.to(Math.floor(this.value));
-  //
-  // });
-
-  var t = false;
-  setInterval(function() {
-    t = !t;
-    if (t) {
-      servo.nw.to(180);
-      servo.ne.to(0);
-      servo.sw.to(0);
-      servo.se.to(180);
-    } else {
-      servo.nw.to(0);
-      servo.ne.to(180);
-      servo.sw.to(180);
-      servo.se.to(0);
-    }
-  }, 2000)
+  plane.init();
 });
---
+++ plane.js
@@ -0,0 +1,46 @@
+var five = require('johnny-five');
+
+var state = 'waiting';
+var scalingRange = [0, 170]
+var servos = {};
+
+module.exports = {
+  init: function() {
+    servos = {
+      nw: new five.Servo({
+        pin: 10,
+        range: scalingRange
+      }),
+      ne: new five.Servo({
+        pin: 12,
+        range: scalingRange
+      }),
+      sw: new five.Servo({
+        pin: 11,
+        range: scalingRange
+      }),
+      se: new five.Servo({
+        pin: 13,
+        range: scalingRange
+      }),
+    }
+    state = 'ready';
+  },
+
+  applyRotation: function(gamma, beta) {
+    if (state === 'ready') {
+      var fixedBeta = beta * 3 > 90 ? 90 : beta * 3 < -90 ? -90 : beta * 3;
+      var fixedGamma = gamma * 3 > 90 ? 90 : gamma * 3 < -90 ? -90 : gamma * 3;
+      var xAxis = (fixedBeta * 1 / 90);
+      var yAxis = (gamfixedGammama * 1 / 90);
+      servos.nw.to(this.rotationToServoValue(xAxis + yAxis));
+      servos.ne.to(this.rotationToServoValue(xAxis - yAxis));
+      servos.sw.to(this.rotationToServoValue(- xAxis + yAxis));
+      servos.se.to(this.rotationToServoValue(- xAxis - yAxis));
+    }
+  },
+
+  rotationToServoValue: function(value) {
+    return value * (scalingRange[1] / 2) + (scalingRange[1] / 2);
+  }
+}
