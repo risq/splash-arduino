894ee023d4c02687ff1c21812bbff8f4543bc7e3
risq <valentin.ledrapier@gmail.com>
2015-10-04 19:50:43 +0200

    Add arduino config

--- config/index.js
+++ config/index.js
@@ -1,4 +1,5 @@
 module.exports = {
   staticFilesPort: 4000,
-  socketServerUrl: 'http://192.168.31.92:5000'
+  socketServerUrl: 'http://192.168.1.29:5000',
+  arduino: true
 }
--- server/arduino.js
+++ server/arduino.js
@@ -1,16 +1,20 @@
 var five = require('johnny-five');
 var plane = require('./plane');
+var config = require('../config');
 
 var state = 'waiting';
 var board, paintMotor;
 
 module.exports = {
   init: function() {
-    board = new five.Board();
-    board.on('ready', this.onBoardReady);
+    if (config.arduino) {
+      board = new five.Board();
+      board.on('ready', this.onBoardReady);
+    }
   },
 
   onBoardReady: function() {
+    console.log('OK')
     plane.init();
     paintMotor = new five.Servo({
       pin: 9,
